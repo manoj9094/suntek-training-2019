47112253603bd68a5b3376c5be7a73d483188759
manoj9094 <55558858+manoj9094@users.noreply.github.com>
2019-09-22 17:51:04 +0530

    Update 16-sept-2019.c

--- 16-sept-2019.c
+++ 16-sept-2019.c
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+/* find the reverse of the array*/
 #include <stdio.h>
-
 int main()
 {
     int i,j,a[10],b[10],n;
