59068b5575da07205a86962a961fadb7e7643530
manoj9094 <55558858+manoj9094@users.noreply.github.com>
2019-09-22 18:05:41 +0530

    Update 17-sept-2019 (2).c

--- 17-sept-2019 (2).c
+++ 17-sept-2019 (2).c
@@ -1,8 +1,9 @@
-
+/*c program to print multiplication table*/
 #include <stdio.h>
 int main()
 {
     int i,j,k;
+    print("enter the table going to print");
     scanf("%d", &i);
     for(j=1;j<=10;j++)
     {
